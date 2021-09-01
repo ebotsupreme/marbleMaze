cbb464885fbf5d81950f8f4e5d6eea92d35e6354
Eddie Jung <ejung716@gmail.com>
2021-09-01 14:18:18 -0700

    Add touch locations.

--- Project26/GameScene.swift
+++ Project26/GameScene.swift
@@ -18,6 +18,7 @@ enum CollisionTypes: UInt32 {
 
 class GameScene: SKScene {
     var player: SKSpriteNode!
+    var lastTouchPosition: CGPoint?
     
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "background")
@@ -28,6 +29,8 @@ class GameScene: SKScene {
         
         loadLevel()
         createPlayer()
+        
+        physicsWorld.gravity = .zero
     }
     
     func loadLevel() {
@@ -116,4 +119,26 @@ class GameScene: SKScene {
         addChild(player)
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        lastTouchPosition = location
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        lastTouchPosition = location
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        lastTouchPosition = nil
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        if let lastTouchPosition = lastTouchPosition {
+            let diff = CGPoint(x: lastTouchPosition.x - player.position.x, y: lastTouchPosition.y - player.position.y)
+            physicsWorld.gravity = CGVector(dx: diff.x / 100, dy: diff.y / 100)
+        }
+    }
 }
